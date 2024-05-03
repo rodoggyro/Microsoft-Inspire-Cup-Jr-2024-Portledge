bed64a02030e05215716c76d9f3f5e593aba56d6
Rowan Shenoy <rshenoy309@gmail.com>
2024-05-03 12:10:12 -0400

    Data population script functions
    Data is also fully populated.
    TODO: Add suggestions and dislikes.

--- csvEditor.py
+++ csvEditor.py
@@ -1,13 +1,64 @@
 import csv
 from random import *
 
+'''
+This script is used to generate a csv file with random data for the healthData.csv file.
+
+Suggestions Key:
+    1. Cut calories
+    2. Excerise more
+Dislikes Key:
+'''
+
 with open('healthData.csv', 'w') as file:
-    for i in csv.reader(file):
-        if (random.randint(0, 1) == 0):
-            data = [i, randint(2400, 3000), randint(83, 115), data[2] * 100, randint(1,10), 'M']
+    writer = csv.writer(file)
+
+    data = []
+    
+    for rows in range(0,501):
+        if(rows == 0):
+            data = [
+                'User Number',
+                'Day Number',
+                'Calories Intake',
+                'Calories Burned',
+                'Sex',
+                'Weight',
+                'Target Weight',
+                'Steps Taken',
+                'Suggestion',
+                'Dislike'
+            ]
+            # User Number ,Day Number ,Calories Intake ,Calories Burned ,Sex ,Weight ,Target Weight ,Steps Taken ,Suggestion ,Dislike
+
         else:
-            data = [i, randint(2000, 2500), randint(66, 81), data[2] * 100, randint(1,10), 'F']
-        file.write(data)
-    reader = csv.reader(file)
-    for row in reader:
-        print(row)
+            if (randint(0, 1) == 0):
+                data = [
+                    rows,
+                    randint(0, 50),
+                    randint(2500, 3000), 
+                    randint(2000, 2450), 
+                    'M', 
+                    randint(197-25, 197+25), 
+                    150, 
+                    randint(500, 15000), 
+                    0,
+                    0
+                ]
+            else:
+                data = [
+                    rows,
+                    randint(0, 50),
+                    randint(1600, 2000), 
+                    randint(1600, 1950), 
+                    'F', 
+                    randint(171-25, 171+25), 
+                    125, 
+                    randint(500, 15000), 
+                    0,
+                    0
+                ]
+        
+        #get the header of a csv file and write it to the file
+
+        writer.writerow(data)
